a9227c54492836c568905ecc5487d0e8e4c01062
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-20 06:31:30 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    Backer.backed_project(self) unless Backer.
+    Backer.backed_project(self) unless Backer.backed_project.include?(self)
     
   end 
   
